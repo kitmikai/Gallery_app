9977cd9f23479f81f0f844856af7200114017fd2
kitmikai <matara.timothy@gmail.com>
2020-07-20 20:48:36 +0300

    Add upload route

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require('express');
 // Import path
 const path = require('path');
 
+// MIDDLEWARES
+// Import multer
+const multer = require('multer');
+
+const upload = require('./upload');
+
 // Initialize express();
 const app = express();
 
@@ -14,7 +20,24 @@ const PORT = 5000;
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './view'));
+app.set('views', path.join(__dirname, './views'));
+
+// Routes
+app.get('/', (request, response) => {
+  response.render('index');
+});
+
+// Route to handle image upload
+app.post('/upload', (request, response) => {
+  upload(request, response, (err) => {
+    if (err) {
+      console.log(err);
+      response.render('index', { msg: err });
+    } else {
+      response.render('index', { file: 'images/' + request.file.filename });
+    }
+  });
+});
 
 app.listen(PORT, function () {
   console.log(`Server is listening on port ${PORT}`);
